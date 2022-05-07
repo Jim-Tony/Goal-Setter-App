8727e8e979a307328be7a5c092645dbe4e470b94
Jim-Tony <jimtonystarc@gmail.com>
2022-05-07 23:43:52 +0530

    user creation done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,40 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 // @desc Register New User
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req,res)=>{
-    res.json({message:'new user'})
+    const {name,email,password} = req.body;
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error('Please fill the details');
+    }
+    //Check if user is already registered
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400);
+        throw new Error('User already registered');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword,
+    })
+    if(user){
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+        })
+    }
+    else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 })
 
 // @desc Authenticate a  User
