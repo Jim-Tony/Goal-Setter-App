1c4e275a6fbd1d3cd0679ea4683a6e25949f067a
Jim-Tony <jimtonystarc@gmail.com>
2022-05-19 20:40:21 +0530

    login comp

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
-
+import {useState,useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa';
 function Login() {
+  const [formData,setFormData] = useState({
+    email:'',
+    password:'',
+  })
+  const {email,password} = formData;
+  const handleChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]:e.target.value,
+    }))
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+  }
   return (
-    <div>Login</div>
+    <>
+      <section className="heading">
+        <h1><FaSignInAlt/> Login</h1>
+        <p>Login to start</p>
+      </section>
+      <section className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              placeholder='Enter your email-id'
+              value={email}
+              autoComplete='off'
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              placeholder='Enter password'
+              value={password}
+              autoComplete='off'
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
-export default Login
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,84 @@
-import React from 'react'
-
+import {useState,useEffect} from 'react'
+import {FaUser} from 'react-icons/fa';
 function Register() {
+  const [formData,setFormData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    password2:'',
+  })
+  const {name,email,password,password2} = formData;
+  const handleChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]:e.target.value,
+    }))
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+  }
   return (
-    <div>Register</div>
+    <>
+      <section className="heading">
+        <h1><FaUser/> Register</h1>
+        <p>Please create an account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              name='name'
+              placeholder='Enter your name'
+              value={name}
+              autoComplete='off'
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              placeholder='Enter your email-id'
+              value={email}
+              autoComplete='off'
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              placeholder='Enter password'
+              value={password}
+              autoComplete='off'
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              placeholder='Confirm password'
+              value={password2}
+              autoComplete='off'
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
