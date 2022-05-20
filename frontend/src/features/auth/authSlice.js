492fdd4ccea100d12442d6de785d83366c9d2baa
Jim-Tony <jimtonystarc@gmail.com>
2022-05-20 15:59:26 +0530

    redux auth start

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,24 @@
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit';
+const user = JSON.parse(localStorage.getItem('user'));
+const initialState = {
+    user:user ? user : null,
+    isSuccess:null,
+    isError:null,
+    isLoading:null,
+    message:''
+}
+export const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        reset:(state)=>{
+            state.isloading = false
+            state.isSuccess = false
+            state.isError = false
+            state.message = ''
+        }
+    },
+    extraReducers:()=>{}
+}) 
+export const {reset} = authSlice.actions
+export default authSlice.reducer
