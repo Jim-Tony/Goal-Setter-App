98decb41e78c29c6391c4c25ad1b1ffd8b8f2f85
Jim-Tony <jimtonystarc@gmail.com>
2022-05-22 13:53:41 +0530

    login redux user

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,24 @@
 import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
+import { useSelector,useDispatch } from 'react-redux';
+import {logout,reset} from '../features/auth/authSlice';
+import {Link,useNavigate} from 'react-router-dom';
 function Header() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {user} = useSelector(state=>state.auth);
+    const onLogout = ()=>{
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/');
+    }
   return (
     <header className='header'>
         <div className="logo">
             <Link to='/'>GoalSetter</Link>
         </div>
         <ul>
+            {user ? (<button className='btn' onClick={onLogout}><FaSignOutAlt/>Logout</button>):(
+            <>
             <li>
                 <Link to='/login'>
                     <FaSignInAlt/> Login
@@ -17,6 +29,8 @@ function Header() {
                     <FaUser/> Register
                 </Link>
             </li>
+            </>  
+            )}
         </ul>
     </header>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,29 @@
 import {useState,useEffect} from 'react'
+import {useSelector,useDispatch} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {login,reset} from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 import {FaSignInAlt} from 'react-icons/fa';
 function Login() {
   const [formData,setFormData] = useState({
     email:'',
     password:'',
   })
+
   const {email,password} = formData;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {user,isLoading,isError,message,isSuccess} = useSelector(state=>state.auth);
+  useEffect(()=>{
+    if(isError){
+      toast.error(message);
+    }
+    if(isSuccess || user){
+      navigate('/')
+    }
+    dispatch(reset())
+  },[user,isError,isSuccess,message,navigate,dispatch])
   const handleChange = (e)=>{
     setFormData((prevState)=>({
       ...prevState,
@@ -14,7 +32,13 @@ function Login() {
   }
   const handleSubmit = (e)=>{
     e.preventDefault();
+    const userData = {
+      email,
+      password,
+    }
+    dispatch(login(userData));
   }
+  if(isLoading) return <Spinner/>;
   return (
     <>
       <section className="heading">
